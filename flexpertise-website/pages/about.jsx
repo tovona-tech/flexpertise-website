e090a85573409ebf531e5cde494b29129195a104
tovona-tech <tovona11@gmail.com>
2025-07-06 17:59:17 +0200

    Delete flexpertise-website directory

--- flexpertise-website/pages/about.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>Unsere Mission: Karriere und Familie vereinbar machen</h1>
-      <p>Flexpertise wurde gegründet, um hochqualifizierten Frauen die Möglichkeit zu geben, ihre Karriere auf höchstem Niveau fortzusetzen...</p>
-    </div>
-  );
-}
--- flexpertise-website/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Ihre Expertise, flexibel eingesetzt</h1>
-      <p>Flexpertise verbindet hochqualifizierte Frauen mit Unternehmen, die flexible Lösungen suchen.</p>
-    </div>
-  );
-}
